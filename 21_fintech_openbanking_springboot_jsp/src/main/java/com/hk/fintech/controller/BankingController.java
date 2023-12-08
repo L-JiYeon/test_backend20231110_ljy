d29ab265d2706c9fa2d31d232d9e719940daa8dd
HKEDU <HKEDU@DESKTOP-D15LC3P.mshome.net>
2023-12-08 14:17:17 +0900

    계좌목록수까지

--- 21_fintech_openbanking_springboot_jsp/src/main/java/com/hk/fintech/controller/BankingController.java
+++ 21_fintech_openbanking_springboot_jsp/src/main/java/com/hk/fintech/controller/BankingController.java
@@ -13,12 +13,16 @@ import java.util.Iterator;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.hk.fintech.apidto.UserMeDto;
 import com.hk.fintech.dtos.UserDto;
+import com.hk.fintech.feignMapper.OpenBankingFeign;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -27,6 +31,9 @@ import jakarta.servlet.http.HttpSession;
 @RequestMapping("/banking")
 public class BankingController {
    
+	@Autowired
+	private OpenBankingFeign openBankingFeign;
+	
    @GetMapping("/main")
    public String main() {
       return "main";
@@ -69,6 +76,28 @@ public class BankingController {
       return result;
    }
    
+   //Feign 적용할 경우
+   //나의정보조회
+   @GetMapping("/myinfo_feign")
+   public String myinfo_feign(HttpServletRequest request,
+		   					  Model model) throws IOException, ParseException {
+	      System.out.println("나의정보조회[계좌목록]");
+	      
+	      // 사용자 일련 번호를 가져오기 위해 session객체 구함
+	      HttpSession session = request.getSession();
+	      UserDto ldto = (UserDto)session.getAttribute("ldto");
+	      int userSeqNo = ldto.getUserseqno(); // 사용자 일련번호
+	      String useraccesstoken = ldto.getUseraccesstoken();
+
+	      //json을 userMeDto에 저장
+	      UserMeDto userMeDto = openBankingFeign.requestUserMe("Bearer "+useraccesstoken, userSeqNo+"");
+	      
+	      //자바객체에 결과값을 저장했으므로 Model에 담아서 JSP로 전달할 수 있다.
+	      model.addAttribute("userMeDto",userMeDto);
+	      System.out.println("계좌목록수:"+userMeDto.getRes_list().size());
+	      return "main";
+	   }
+   
    @ResponseBody
    @GetMapping("/balance")
    public JSONObject balance(String fintech_use_num, HttpServletRequest request) throws ParseException, IOException {
@@ -102,6 +131,63 @@ public class BankingController {
       System.out.println("잔액:"+result.get("balance_amt"));
 
       return result;
+   }
+   
+   //거래내역조회
+   @ResponseBody
+   @GetMapping("/transactionList")
+   public JSONObject transactionList(String fintech_use_num, HttpServletRequest request) throws ParseException, IOException {
+      System.out.println("거래내역조회하기");
+      HttpURLConnection conn = null;
+      JSONObject result = null;
+      
+      HttpSession session = request.getSession();
+      UserDto ldto = (UserDto)session.getAttribute("ldto");
+      
+      URL url = new URL("https://testapi.openbanking.or.kr/v2.0/account/transaction_list/fin_num?"
+    		  			+ "bank_tran_id=M202201886U"+createNum()
+    		  			+ "&fintech_use_num="+fintech_use_num 
+    		  			+ "&inquiry_type=A"
+    		  			+ "&inquiry_base=D"
+    		  			+ "&from_date=20190101"
+    		  			+ "&to_date=20190501"
+    		  			+ "&sort_order=D"
+    		  			+ "&tran_dtime="+getDateTime());
+      
+      conn = (HttpURLConnection)url.openConnection();
+      conn.setRequestMethod("GET");
+      conn.setRequestProperty("Content-Type", "application/json");
+      conn.setRequestProperty("Authorization", "Bearer "+ ldto.getUseraccesstoken());
+      conn.setDoOutput(true);
+      
+      // java에서 사용할 수 있도록 읽어들이는 코드
+      BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
+      
+      StringBuilder response = new StringBuilder();
+      String responseLine = null;
+      
+      while((responseLine = br.readLine())!=null) {
+         response.append(responseLine.trim());
+      }
+      
+      //읽은 값이 json 형태로 된 문자열 --> json 객체로 변환하자
+      result = (JSONObject)new JSONParser().parse(response.toString());
+      System.out.println("거래내액:"+result.get("res_list"));
+
+      return result;
+   }
+   
+   //계좌등록
+   @ResponseBody
+   @GetMapping("/addaccount")
+   public String addAccount(String code) {
+	   System.out.println("계좌등록하기");
+	   System.out.println("code: "+code);
+	   //팝업창을 닫아주기 위해서
+	   String str="<script type='text/javascript'>"
+			   	  + "	self.close();"
+			   	  + "</script>";
+	   return str;
    }
    
    // 이용기관 부여번호 9자리를 생성하는 메서드
